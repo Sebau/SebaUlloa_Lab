e174203ef9b9f7227c65229bc6b0159ef9000336
seba <uyos92@gmail.com>
2020-10-28 00:23:31 -0300

    Ejecicio 2

---
+++ Ejercicio2/Logic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    public class Logic : Exception
+    {
+        public static void CampoVacio()
+        {
+            Console.WriteLine(" Y Yoda Sabe", "Eskere");
+        }
+    }
+}
---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Introduzca un Numero");
+            
+            int numUno = int.Parse(Console.ReadLine());
+
+            MyExtensions.DivisionN(numUno);
+
+            Console.ReadKey();
+
+            //ACA PASO AL SEGUNDO NUMERO
+            Console.ReadKey();
+
+            Console.WriteLine("Introduzca un segundo numero");
+
+            int numDos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduzca un tercer numero");
+
+            int numTres = int.Parse(Console.ReadLine());
+
+                try 
+                {
+
+                    MyExtensions.Division(numDos, numTres);
+                }
+                catch
+                {
+                    Console.WriteLine("Solamente Chuck Norris Divide Por 0");
+                }
+            Logic.CampoVacio();        
+            Console.ReadKey();
+        }
+    }
+}
